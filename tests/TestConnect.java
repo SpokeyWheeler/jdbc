e247184ee5191774a36022aa31800e102e0ea13b
William Wheeler <william.wheeler@sgdigital.com>
2019-02-12 14:10:36 +0000

    Add Java connection test

---
+++ tests/TestConnect.java
@@ -0,0 +1,65 @@
+import java.sql.*;
+import java.util.*;
+import java.lang.Thread;
+
+public class TestConnect {
+
+    public static void main(String[] args)
+    {
+        if (args.length == 0)
+            {
+            System.out.println("ERROR: connection URL must be provided in order to run the demo!");
+            return;
+            }
+
+        String url = args[0];
+
+        String testName = "Simple Connection";
+
+        Connection conn = null;
+
+        System.out.println(">>>" + testName + " test.");
+        System.out.println("URL = \"" + url + "\"");
+
+        try 
+            {
+	    Class.forName("com.informix.jdbc.IfxDriver");
+            //Class.forName("com.cloudscape.core.JDBCDriver");
+            } 
+        catch (Exception e)
+            {
+            System.out.println("ERROR: failed to load Informix JDBC driver.");
+            }
+
+        try 
+            {
+            conn = DriverManager.getConnection(url);
+	    System.out.println("Connected...");
+            } 
+        catch (SQLException e) 
+            {
+            System.out.println("ERROR: failed to connect!");
+            }
+
+	try
+	    {
+		Thread.sleep (5000);
+	    }
+        catch (InterruptedException i) 
+            {
+            System.out.println("Sleep interupted");
+            }
+
+        try 
+            {
+            conn.close();
+            } 
+        catch (SQLException e) 
+            {
+            System.out.println("ERROR: failed to close the connection!");
+            }
+
+        System.out.println(">>>End of " + testName + " test.");
+    }
+}
+
